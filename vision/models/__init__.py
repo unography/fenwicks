58c7da1818814bad15acbf882d72e4e676198af2
Dhruv Karan <k4r4n.dhruv@gmail.com>
2019-08-07 00:06:14 +0530

    trying to fix 'set_keras_submodules' error
    AttributeError: module 'keras_applications' has no attribute 'set_keras_submodules'

--- vision/models/__init__.py
+++ vision/models/__init__.py
@@ -1,6 +1,27 @@
-import tensorflow as tf
 import keras_applications
-from tensorflow.python.keras import engine as keras_engine
 
-keras_applications.set_keras_submodules(backend=tf.keras.backend, layers=tf.keras.layers, models=tf.keras.models,
-                                        utils=tf.keras.utils, engine=keras_engine)
+from tensorflow.python.keras import backend
+from tensorflow.python.keras import engine
+from tensorflow.python.keras import layers
+from tensorflow.python.keras import models
+from tensorflow.python.keras import utils
+from tensorflow.python.util import tf_inspect
+
+
+def keras_modules_injection(base_fun):
+  """Decorator injecting tf.keras replacements for Keras modules.
+  Arguments:
+      base_fun: Application function to decorate (e.g. `MobileNet`).
+  Returns:
+      Decorated function that injects keyword argument for the tf.keras
+      modules required by the Applications.
+  """
+
+  def wrapper(*args, **kwargs):
+    kwargs['backend'] = backend
+    if 'layers' not in kwargs:
+      kwargs['layers'] = layers
+    kwargs['models'] = models
+    kwargs['utils'] = utils
+    return base_fun(*args, **kwargs)
+  return wrapper
